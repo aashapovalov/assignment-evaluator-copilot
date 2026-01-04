2df43e30bfe6fe5e1667cbc51d132d861710b955
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-04 12:46:59 +0200

    refactor: create index.js for importing logos and icons

---
+++ frontend/src/assets/logo/index.js
@@ -0,0 +1,21 @@
+import app_logo_dark from './app_logo_dark.png';
+import app_logo_light from './app_logo_light.png';
+import app_title_dark from './app_title_dark.png';
+import app_title_light from './app_title_light.png';
+import btn_upload_ipynb from './btn_upload_ipynb.png';
+import btn_upload_txt from './btn_upload_txt.png';
+import icon_get_evaluation from './icon_get_evaluation.png';
+import icon_read_assignment from './icon_read_assignment.png';
+import icon_read_solution from './icon_read_solution.png';
+
+export {
+  app_logo_dark,
+  app_logo_light,
+  app_title_dark,
+  app_title_light,
+  btn_upload_ipynb,
+  btn_upload_txt,
+  icon_get_evaluation,
+  icon_read_assignment,
+  icon_read_solution
+};
